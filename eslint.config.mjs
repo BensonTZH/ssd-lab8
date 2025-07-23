18043de797ad6c6c8cb1d5def6788dd95556cda7
bensonTZH <bensontan.tp@gmail.com>
2025-07-23 23:34:16 +0800

    lol

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,11 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-import pluginSecurity from "eslint-plugin-security";
 import { defineConfig } from "eslint/config";
 
+
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    plugins: { js, security: pluginSecurity, react: pluginReact },
-    extends: ["js/recommended", "plugin:react/recommended", "plugin:security/recommended"],
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 2021,
-        sourceType: "module",
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
-    rules: {
-      "security/detect-eval-with-expression": "error",
-    },
-  },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
 ]);
