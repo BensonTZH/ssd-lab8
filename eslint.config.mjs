457456fe5c35f6def2bdf7b580ae41838cfa5656
bensonTZH <bensontan.tp@gmail.com>
2025-07-23 23:30:23 +0800

    lol

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,32 @@
-import js from "@eslint/js";
 import globals from "globals";
+import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
+/** @type {import('eslint').FlatConfig[]} */
+const config = [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      security: pluginSecurity,
+      react: pluginReact,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...pluginReact.configs.flat.recommended.rules,
+      "security/detect-eval-with-expression": "error",
+    },
+  },
+];
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
+export default config;
